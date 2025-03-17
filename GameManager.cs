d19e67fdef4502a67381adadc43682cceb15d3d8
Scott Burton <burtons@byui.edu>
2025-03-17 12:41:14 -0600

    Minor refactor and updated comments.

--- GameManager.cs
+++ GameManager.cs
@@ -2,22 +2,27 @@ using Raylib_cs;
 
 class GameManager
 {
-    private int _width;
-    private int _height;
+    public const int SCREEN_WIDTH = 800;
+    public const int SCREEN_HEIGHT = 600;
+
     private string _title;
 
     public GameManager()
     {
-        _width = 800;
-        _height = 480;
         _title = "CSE 210 Game";
     }
 
+    /// <summary>
+    /// The overall loop that controls the game. It calls functions to
+    /// handle interactions, update game elements, and draw the screen.
+    /// </summary>
     public void Run()
     {
-        InitializeGame();
+        Raylib.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, _title);
+        // If using sound, you can un-comment this line and the one below.
+        // Raylib.InitAudioDevice();
 
-        Raylib.InitWindow(_width, _height, _title);
+        InitializeGame();
 
         while (!Raylib.WindowShouldClose())
         {
@@ -32,24 +37,37 @@ class GameManager
             Raylib.EndDrawing();
         }
 
+        // Raylib.CloseAudioDevice();
         Raylib.CloseWindow();
     }
 
+    /// <summary>
+    /// Sets up the initial conditions for the game.
+    /// </summary>
     private void InitializeGame()
     {
 
     }
 
+    /// <summary>
+    /// Responds to any input from the user.
+    /// </summary>
     private void HandleInput()
     {
 
     }
 
+    /// <summary>
+    /// Processes any actions such as moving objects or handling collisions.
+    /// </summary>
     private void ProcessActions()
     {
 
     }
 
+    /// <summary>
+    /// Draws all elements on the screen.
+    /// </summary>
     private void DrawElements()
     {
 
